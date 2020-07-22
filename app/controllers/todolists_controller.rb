9653dbdaee75e2a2dab9c4b6206f0a28e82d56f7
Apple <apple@appurunoMacBook-puro.local>
2020-07-22 16:06:52 +0900

    3-10

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,48 @@
+class TodolistsController < ApplicationController
+  def new
+  	# Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+  	@list = List.new
+  end
+
+def create
+	#データを新規登録するためのインスタンス作成
+	list = List.new(list_params)
+	#データをデータベースに保存
+	list.save
+	
+	redirect_to todolist_path(list.id)
+end
+
+def index
+	@lists = List.all
+end
+
+def show
+	@list = List.find(params[:id])
+end
+
+def edit
+	@list = List.find(params[:id])
+end
+
+def update
+	list = List.find(params[:id])
+	list.update(list_params)
+	redirect_to todolist_path(list.id)
+end
+
+def destroy
+	list = List.find(params[:id])
+	list.destroy
+	redirect_to todolists_path
+end
+
+
+private
+
+def list_params
+params.require(:list).permit(:title, :body, :image)
+end
+
+end
+
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e17819a53ad044352a14c1a046c0e103e51e176dcf63cf4c0817e6f53bcea61e2c331f3e568b8d21712deb437a94709f0b93b62b68cdf62ca9e5b2e439bf8a9a'
